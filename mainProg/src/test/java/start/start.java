695717d7cda354592847303a6c521250bdf438dc
mslaby <mateusz.s00@outlook.com>
2015-03-26 10:42:08 +0100

    będzie

--- mainProg/src/test/java/start/start.java
+++ mainProg/src/test/java/start/start.java
@@ -5,6 +5,6 @@ package start;
  */
 public class start {
     public static void main(String[] args) {
-    System.out.println("Start! Bedzie zabawa");
+    System.out.println("Start! Bedzie zabawa, bedzie sie dzialo");
     }
 }
