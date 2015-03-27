2cdbbe5bbda299f818742518567a2ddccc9d839e
mms1994 <michal.spirala@hotmail.com>
2015-03-27 21:33:10 +0100

    baza start

--- mainProg/src/test/java/baza/danych.java
+++ mainProg/src/test/java/baza/danych.java
@@ -1,13 +1,19 @@
 package baza;
 
+/*
+SCHEMAT ZAPISU DANYCH DO TABLICY
+ID|NAZWA|CENA
+*/
 public class danych {
-    public String [][]obudowy;
-    public String [][]procesory;
-    public String [][]plyty_glowne;
-    public String [][]RAM;
-    public String [][]akcesoria;
-    public String [][]dyski;
-    public String [][]karty_graficzne;
-    public String [][]karty_dzwiekowe;
-    public String [][]zasilacze;
+    public String [][]obudowy=new String [1][3];
+    public String [][]procesory=new String [1][3];
+    public String [][]plyty_glowne=new String [1][3];
+    public String [][]RAM=new String [1][3];
+    public String [][]akcesoria=new String [1][3];
+    public String [][]dyski=new String [1][3];
+    public String [][]karty_graficzne=new String [1][3];
+    public String [][]karty_dzwiekowe=new String [1][3];
+    public String [][]zasilacze=new String [1][3];
+    public String [][]system=new String [1][3];
+    public String [][]oprogramowanie=new String [1][3];
 }
