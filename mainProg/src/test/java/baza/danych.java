086ffbcbb9c20fff4ab931d16e9f74ea03a58971
mms1994 <michal.spirala@hotmail.com>
2015-03-27 21:23:30 +0100

    readme

---
+++ mainProg/src/test/java/baza/danych.java
@@ -0,0 +1,12 @@
+package baza;
+
+public class danych {
+    public String [][]obudowy;
+    public String [][]procesory;
+    public String [][]plyty_glowne;
+    public String [][]RAM;
+    public String [][]akcesoria;
+    public String [][]dyski;
+    public String [][]karty_graficzne;
+    public String [][]karty_dzwiekowe;
+}
