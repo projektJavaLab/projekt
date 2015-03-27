5f26ba1913f9b89fafb6050485a8d8a96a99eedf
mms1994 <michal.spirala@hotmail.com>
2015-03-27 23:14:27 +0100

    dodanie bazy oraz wczytywanie bazy do tabeli

---
+++ mainProg/src/test/java/start/baza/danych.java
@@ -0,0 +1,20 @@
+package start.baza;
+
+/*
+SCHEMAT ZAPISU DANYCH DO TABLICY
+ID|NAZWA|CENA
+*/
+public class danych {
+
+    public static String [][]obudowy=new String [2][3];
+    public static String [][]procesory=new String [2][3];
+    public static String [][]plyty_glowne=new String [2][3];
+    public static String [][]RAM=new String [2][3];
+    public static String [][]akcesoria=new String [2][3];
+    public static String [][]dyski=new String [2][3];
+    public static String [][]karty_graficzne=new String [2][3];
+    public static String [][]karty_dzwiekowe=new String [2][3];
+    public static String [][]zasilacze=new String [2][3];
+    public static String [][]systemy=new String [2][3];
+    public static String [][]oprogramowanie=new String [2][3];
+}
--- mainProg/src/test/java/start/start.java
+++ mainProg/src/test/java/start/start.java
@@ -1,7 +1,88 @@
 package start;
 
+import start.baza.danych;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class start {
-    public static void main(String[] args) {
-    System.out.println("Start");
+    public static void main(String[] args) throws FileNotFoundException {
+        // wczytanie do bazy z plików listy części
+        // wczytywanie obudów
+        Scanner skaner_obudowy = new Scanner(new BufferedReader(new FileReader("obudowy.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_obudowy.hasNext())
+                danych.obudowy[i][j] = skaner_obudowy.next();
+        Scanner skaner_procesory = new Scanner(new BufferedReader(new FileReader("procesory.txt")));
+        // wczytywanie procesorów
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_procesory.hasNext())
+                danych.procesory[i][j] = skaner_procesory.next();
+        // wczytywanie płyt głównych
+        Scanner skaner_plyty_glowne = new Scanner(new BufferedReader(new FileReader("plyty_glowne.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_plyty_glowne.hasNext())
+                    danych.plyty_glowne[i][j] = skaner_plyty_glowne.next();
+        // wczytywanie kości RAM
+        Scanner skaner_RAM = new Scanner(new BufferedReader(new FileReader("RAM.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_RAM.hasNext())
+                    danych.RAM[i][j] = skaner_RAM.next();
+        // wczytywanie akcesoriów
+        Scanner skaner_akcesoria = new Scanner(new BufferedReader(new FileReader("akcesoria.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_akcesoria.hasNext())
+                    danych.akcesoria[i][j] = skaner_akcesoria.next();
+        // wczytywanie dysków
+        Scanner skaner_dyski = new Scanner(new BufferedReader(new FileReader("dyski.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_dyski.hasNext())
+                    danych.dyski[i][j] = skaner_dyski.next();
+        // wczytywanie kart graficznych
+        Scanner skaner_karty_graficzne = new Scanner(new BufferedReader(new FileReader("karty_graficzne.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_karty_graficzne.hasNext())
+                    danych.karty_graficzne[i][j] = skaner_karty_graficzne.next();
+        // wczytywanie kart dźwiękowych
+        Scanner skaner_karty_dzwiekowe = new Scanner(new BufferedReader(new FileReader("karty_dzwiekowe.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_karty_dzwiekowe.hasNext())
+                    danych.karty_dzwiekowe[i][j] = skaner_karty_dzwiekowe.next();
+        // wczytywanie zasilaczy
+        Scanner skaner_zasilacze = new Scanner(new BufferedReader(new FileReader("zasilacze.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_zasilacze.hasNext())
+                    danych.zasilacze[i][j] = skaner_zasilacze.next();
+        // wczytywanie systemów
+        Scanner skaner_systemy = new Scanner(new BufferedReader(new FileReader("systemy.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_systemy.hasNext())
+                    danych.systemy[i][j] = skaner_systemy.next();
+        // wczytywanie oprogramowania
+        Scanner skaner_oprogramowanie = new Scanner(new BufferedReader(new FileReader("oprogramowanie.txt")));
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                if(skaner_oprogramowanie.hasNext())
+                    danych.oprogramowanie[i][j] = skaner_oprogramowanie.next();
+        wypisanie();
+        //
+    }
+    public static void wypisanie() {
+        String str = danych.procesory[1][1];
+        System.out.println(str);
+        String str2 = danych.obudowy[1][1];
+        System.out.println(str2);
     }
 }
