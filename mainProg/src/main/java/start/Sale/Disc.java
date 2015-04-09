32139d363f3eded38760e3cd1ab19f3c90d26e9e
mms1994 <michal.spirala@hotmail.com>
2015-04-09 10:36:26 +0200

    Resume

---
+++ mainProg/src/main/java/start/Sale/Disc.java
@@ -0,0 +1,7 @@
+package start.Sale;
+
+/**
+ * Created by Michał on 2015-04-09.
+ */
+public class Disc {
+}
---
+++ mainProg/src/main/java/start/Sale/Payment.java
@@ -0,0 +1,7 @@
+package start.Sale;
+
+/**
+ * Created by Michał on 2015-04-09.
+ */
+public class Payment {
+}
