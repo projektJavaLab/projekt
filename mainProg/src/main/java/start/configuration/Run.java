8fca5091ef6463f62b67c700a83bd2d245ae221d
mms1994 <michal.spirala@hotmail.com>
2015-04-11 14:15:32 +0200

    Moc

--- mainProg/src/main/java/start/configuration/Run.java
+++ mainProg/src/main/java/start/configuration/Run.java
@@ -5,6 +5,8 @@ public class Run {
     double Price=0;
     // tablica przechowująca indexy do częsci
     String Konf[]=new String[8];
+    // potrzebna moc
+    double Power=0;
     // funkcja konfigurująca
     public static void Start() {
 
