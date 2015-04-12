6014283dfad4fb79f56106d09ffc60f07a6a7866
krprzenioslo <zastow44>
2015-04-12 13:05:09 +0200

    KP RAM i obudowy 1 up (prawdopodobnie zly)

--- mainProg/src/main/java/start/configuration/Case.java
+++ mainProg/src/main/java/start/configuration/Case.java
@@ -1,16 +1,38 @@
 package start.configuration;
 
+import start.baza.danych;
+import start.output;
+
+import java.util.Scanner;
+
 public class Case extends Main{
     public void List() {
+        System.out.println("ID || PRODUCENT || MODEL || PORTY 525" || PORTY 35" || MAX WYS. || CHLODZENIA || STAN MAGAZYNOWY || CENA");
+        output.writeStringTab(danych.CASE);
     }
     public double Price(int id) {
-        double price = 0;
-        return price;
+        String p = danych.CASE[id][2];
+        return Double.parseDouble(p);
     }
     public int ID() {
         int id=0;
-        return id;
+        System.out.println("Podaj ID części, w celu zakończenia podaj 0");
+        Scanner in = new Scanner(System.in);
+        id = in.nextInt();
+        return id-1;
     }
-    public void Title() {
+    public String Acc() {
+        String Sid="";
+        List();
+        int temp;
+        while (true){
+            temp = ID();
+            if(temp==-1) break;
+            Sid += temp;
+            Sid += ",";
+            Sid += Price(temp);
+            Sid += ";";
+        }
+        return Sid;
     }
 }
--- mainProg/src/main/java/start/configuration/RAM.java
+++ mainProg/src/main/java/start/configuration/RAM.java
@@ -1,16 +1,39 @@
 package start.configuration;
 
+import start.baza.danych;
+import start.output;
+
+import java.util.Scanner;
+
 public class RAM extends Main{
     public void List() {
+        System.out.println("ID || TYP || PRODUCENT || ROZMIAR || TAKTOWANIE || STAN MAGAZYNOWY || CENA");
+        output.writeStringTab(danych.RAM);
     }
+
     public double Price(int id) {
-        double price = 0;
-        return price;
+        String p = danych.RAM[id][2];
+        return Double.parseDouble(p);
     }
     public int ID() {
         int id=0;
-        return id;
+        System.out.println("Podaj ID części, w celu zakończenia podaj 0");
+        Scanner in = new Scanner(System.in);
+        id = in.nextInt();
+        return id-1;
     }
-    public void Title() {
+    public String Acc() {
+        String Sid="";
+        List();
+        int temp;
+        while (true){
+            temp = ID();
+            if(temp==-1) break;
+            Sid += temp;
+            Sid += ",";
+            Sid += Price(temp);
+            Sid += ";";
+        }
+        return Sid;
     }
 }
