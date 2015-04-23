d7f88bd9252bd0085481c6cfb3056ec4e238fc6e
mms1994 <michal.spirala@hotmail.com>
2015-04-23 10:22:14 +0200

    new branch test

--- mainProg/src/test/test.java
+++ mainProg/src/test/test.java
@@ -5,5 +5,9 @@ import start.configuration.Run;
  * Created by Michał on 2015-04-23.
  */
 public class test {
-
+    @org.junit.Test
+    public void testName() throws Exception {
+        Run run = new Run();
+        Assert.assertNotNull(run.Konf);
+    }
 }
