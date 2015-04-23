f465d9fad5627dfb9b41cab9ae8c3c18f2d210a4
KP <kprenioslo@poczta.fm>
2015-04-23 10:12:25 +0200

    Sposob zapłaty ver 0.1 pre alpha

--- mainProg/src/main/java/start/Sale/Payment.java
+++ mainProg/src/main/java/start/Sale/Payment.java
@@ -4,4 +4,11 @@ package start.Sale;
  * Created by Michał on 2015-04-09.
  */
 public class Payment {
+    String[] platnosc=new String[3]; // 0 -gotowka, 1 - karta 2 - przelew
+    public void wybierz_platnosc()
+    {
+        System.out.println("Wybierz sposób zapłaty: "+ "\n" + "1 - gotowka" + "\n" + "2- karta" + "\n" + "3 - przelew bankowy");
+
+
+    }
 }
