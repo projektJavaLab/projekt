e75818fba33720f2707c0f8197b6b041b61b0740
piotrstrzesa <stpi@o2.pl>
2015-05-07 10:45:39 +0200

    B1-RAM

--- mainProg/src/main/java/start/configuration/RAM.java
+++ mainProg/src/main/java/start/configuration/RAM.java
@@ -19,7 +19,20 @@ public class RAM extends Main{
         int id=0;
         System.out.println("Podaj ID części, w celu zakończenia podaj 0");
         Scanner in = new Scanner(System.in);
-        id = in.nextInt();
+        try
+        {
+            id=Integer.parseInt(in.next());
+        }
+        catch (NumberFormatException n)
+        {
+            System.out.println("Niepoprawne dane !!!");
+            return ID();
+        }
+        if(danych.RAM.length < id || id < 0)
+        {
+            System.out.println("Podano numer z poza zakresu, wybierz ID z podanego zbioru");
+            return ID();
+        }
         return id-1;
     }
     public String Do() {
