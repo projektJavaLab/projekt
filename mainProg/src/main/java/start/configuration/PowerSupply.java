dc77e503bd11fb0db87d40cd2adeafbc05c7dc3f
piotrstrzesa <stpi@o2.pl>
2015-05-10 18:58:41 +0200

    B3-PowerSupply

--- mainProg/src/main/java/start/configuration/PowerSupply.java
+++ mainProg/src/main/java/start/configuration/PowerSupply.java
@@ -16,7 +16,7 @@ public class PowerSupply extends Main{
     }
     public int ID() {
         int id=0;
-        System.out.println("Podaj ID części, w celu zakończenia podaj 0");
+        System.out.println("Podaj ID części");
         Scanner in = new Scanner(System.in);
         try
         {
@@ -27,7 +27,7 @@ public class PowerSupply extends Main{
             System.out.println("Niepoprawne dane !!!");
             return ID();
         }
-        if(danych.POWERSUPPLY.length < id || id < 0)
+        if(danych.POWERSUPPLY.length < id || id < 1)
         {
             System.out.println("Podano numer z poza zakresu, wybierz ID z podanego zbioru");
             return ID();
