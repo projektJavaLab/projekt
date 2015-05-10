a493b86ac6b17bd60f3057bb19ca51d6a03de916
piotrstrzesa <stpi@o2.pl>
2015-05-10 18:15:58 +0200

    B1-HardDrive

--- mainProg/src/main/java/start/configuration/HardDrive.java
+++ mainProg/src/main/java/start/configuration/HardDrive.java
@@ -18,7 +18,20 @@ public class HardDrive extends Main{
         int id=0;
         System.out.println("Podaj ID części, w celu zakończenia podaj 0");
         Scanner in = new Scanner(System.in);
-        id = in.nextInt();
+        try
+        {
+            id=Integer.parseInt(in.next());
+        }
+        catch (NumberFormatException n)
+        {
+            System.out.println("Niepoprawne dane !!!");
+            return ID();
+        }
+        if(danych.HARDDRIVE.length < id || id < 0)
+        {
+            System.out.println("Podano numer z poza zakresu, wybierz ID z podanego zbioru");
+            return ID();
+        }
         return id-1;
     }
     public String Hdd() {
