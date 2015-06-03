b21d35057f5ca9bdea93b98efd4c25d0a5e31051
VuYeK <vuyek04@gmail.com>
2015-06-03 19:40:44 +0200

    gaming, akcesoria, małe poprawki + monitor bo to w zasadzie to samo

--- mainProg/src/main/java/start/dynamic/Main.java
+++ mainProg/src/main/java/start/dynamic/Main.java
@@ -63,6 +63,7 @@ public class Main {
 
         //aktualizacja konfiguracji o wybrane dodatki
         Konf[6]=KonfAdds[6];
+        Konf[7]=KonfAdds[7];
         Konf[12]=KonfAdds[12];
         Konf[13]=KonfAdds[13];
         Konf[15]= String.valueOf(parseDouble(Konf[15])+parseDouble(KonfAdds[15]));
