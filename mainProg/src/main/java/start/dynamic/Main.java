cefcf993a8beb65a2d27b88558f3c6489b6316b4
mms1994 <michal.spirala@hotmail.com>
2015-06-13 12:44:33 +0200

    up

--- mainProg/src/main/java/start/dynamic/Main.java
+++ mainProg/src/main/java/start/dynamic/Main.java
@@ -1,9 +1,6 @@
 package start.dynamic;
 
 public abstract class Main {
-    //
-    //public abstract void List();
-    //public abstract double Price(int id);
     public abstract int ID(double maxPrice, int []tab, String [][]tab2, int C, double P);
     public abstract int ID2();
     public abstract String[] Start(double maxPrice);
